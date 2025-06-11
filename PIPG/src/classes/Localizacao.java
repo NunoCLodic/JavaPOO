52e3932c2e329e2b11af8d4f8c52886a17077675
NunoClodic Carvalho Lima <nunoinformaticaipg@gmail.com>
2025-06-11 23:04:53 +0100

    localizacao
    id na localizacao

--- PIPG/src/classes/Localizacao.java
+++ PIPG/src/classes/Localizacao.java
@@ -1,21 +1,26 @@
-
 package classes;
 
 import java.awt.geom.Point2D;
 
-
 class Localizacao {
+
+    int id;
+    private static int contadorId = 1;
     private Point2D coordenadas;
+
     //construtor localizacao
-    public Localizacao(double latitude, double longitude){
+    public Localizacao(double latitude, double longitude) {
         this.coordenadas = new Point2D.Double(latitude, longitude);
+        this.id = contadorId;
+        contadorId++;
     }
+
     //get latitude e longetude
-    public double getLatitude(){
+    public double getLatitude() {
         return coordenadas.getX();
     }
-    
-    public double getLongitude(){
+
+    public double getLongitude() {
         return coordenadas.getY();
     }
 
